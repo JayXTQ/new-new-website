e0d66031a80e023f5bde15e76847be34f05dea70
Jay <me@jayxtq.xyz>
2024-12-02 13:20:11 +0000

    style(projects): fixed :3

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -22,10 +22,10 @@
     }}
 />
 
-<HeadingBox title="Projects" class="max-h-[50rem] max-w-[100rem] w-full h-full pb-5">
-    <div class="overflow-y-scroll h-full mb-5">
+<HeadingBox title="Projects" class="max-h-[51rem] max-w-[100rem] w-full h-full">
+    <div class="overflow-y-scroll h-full">
         <a href="/" class="float-right">Go back</a>
-        <div class="my-10 space-y-4">
+        <div class="mt-10 space-y-4">
             {#each data.repos as repo}
                 <a href="https://github.com/{repo.author}/{repo.name}" class="block rounded-[13px] p-4 h-auto w-full border-2 border-[#7A7A7A]">
                     <h1 class="font-bold text-xl">{repo.name}</h1>
